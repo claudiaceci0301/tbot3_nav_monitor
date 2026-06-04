43ca94773732b98c8d9434a29432af16a8e10247
claudiaceci0301 <claudiaceci047@gmail.com>
2026-06-04 18:10:42 +0200

    nav2 yaml file updated

--- start.sh
+++ start.sh
@@ -9,7 +9,10 @@ sleep 8
 ros2 launch turtlebot3_cartographer cartographer.launch.py use_sim_time:=true &
 sleep 5
 
-ros2 launch nav2_bringup navigation_launch.py slam:=True use_sim_time:=True &
+ros2 launch nav2_bringup navigation_launch.py \
+  slam:=False \
+  use_sim_time:=True \
+  params_file:=/workspace/src/tbot3_nav_monitor/config/params.yaml &
 sleep 8
 
 ros2 launch tbot3_nav_monitor tbot3_nav_monitor.launch.py
