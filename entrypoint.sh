f2d87f112efeeac3252be7606a00562a406a585b
claudiaceci0301 <claudiaceci047@gmail.com>
2026-06-05 19:38:20 +0200

    last improvements

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,15 @@
+
 #!/bin/bash
 set -e
 
+# ROS2
 source /opt/ros/humble/setup.bash
 
+cd /workspace
+
+# if workspace exist, source it
 if [ -f /workspace/install/setup.bash ]; then
     source /workspace/install/setup.bash
-    echo "workspace sourced"
 fi
 
 exec "$@"
